a13e5d594066a83846a7fe28350f3da93bb5e408
namanvijayv <116099181+namanvijayv@users.noreply.github.com>
2023-08-19 12:17:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://ravi:HelloWorld%401234@rechargeapp.9uks18k.mongodb.net/raga', {
+mongoose.connect('mongodb+srv://taxedir425:ZaWuSALIncHwl5e3@cluster0.o9fzrup.mongodb.net/Raga', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
