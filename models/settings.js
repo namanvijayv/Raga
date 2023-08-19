18c827f9f63635672ba14a3ebf3297ece5bb8253
namanvijayv <116099181+namanvijayv@users.noreply.github.com>
2023-08-19 16:02:34 +0530

    Update settings.js

--- models/settings.js
+++ models/settings.js
@@ -3,14 +3,6 @@ const mongoose = require('../db');
 const userSchema = new mongoose.Schema({
   mold: { type: String, required: true },
   monitorNumber: { type: String, required: true },
-//   age: {
-//     type     : Number,
-//     required : true,
-//     validate : {
-//       validator : Number.isInteger,
-//       message   : '{VALUE} is not an integer value'
-//     }
-//   },
   systemNumber: { type: String, required: true },
   core: { type: Number, required: true },
   corePins: { type: Number, required: true },
