07f34e1c7e5bd61845a311b898df989dce6c0bf7
namanvijayv <116099181+namanvijayv@users.noreply.github.com>
2023-08-21 16:32:54 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const mongoose = require('../db.js');
 const userSchema = new mongoose.Schema({
   companyCode: { type: String, required: true },
   username: { type: String, required: true },
+  email: { type: String, required: true },
+  resetToken: { type: String, required: false },
   password: { type: String, required: true }
 }, { collection: 'login_data' }); 
 
