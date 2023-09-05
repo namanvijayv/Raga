4fbec18904dd513f28df06e6cc9a850054567f18
namanvijayv <116099181+namanvijayv@users.noreply.github.com>
2023-09-05 15:17:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/csv-data", async (req, res) => {
 
   ftpClient.on("ready", () => {
     // Path to the CSV file on the FTP server
-    const remoteFilePath = "/RAGA/26-08-2023/RAGA_26-08-2023.csv";
+    const remoteFilePath = "/RAGA/05-09-2023/RAGA_05-09-2023.csv";
 
     // Read the existing CSV file and process data
     ftpClient.get(remoteFilePath, (err, stream) => {
